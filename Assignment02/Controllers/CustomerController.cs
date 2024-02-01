3c8490cd11083017e8fde1975f324d5d337036e8
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-01 16:43:33 +0530

    Add project files.

---
+++ Assignment02/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using Assignment02.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace Assignment02.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>() {
+        "Naveen","Jon","Sam","Leo","Fiz"
+        };
+            ViewBag.CustList = list;
+            return View();
+        }
+        public ActionResult RedirectDemo() {
+
+            TempData["Msg"] = "welcome to Customer MVC";
+            return RedirectToAction("DisplayMessage");
+        }
+        public ActionResult DisplayMessage() {
+
+            string message = TempData["Msg"] as string;
+            ViewBag.Message = message;
+            return View();
+        }
+    }
+}
